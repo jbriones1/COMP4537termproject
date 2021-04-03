45a4e5833b0efd5419cf947c9dc6c5aa5c47da59
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-03 15:04:18 -0700

    Added cors

--- server.js
+++ server.js
@@ -2,16 +2,19 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
-app.all('*', (req, res, next) => {
+// app.all('*', (req, res, next) => {
 
-	res.set('Access-Control-Allow-Origin', '*');
-	res.set('Access-Control-Allow-Methods', 'GET,POST,PUT');
-	res.set('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-});
+// 	res.set('Access-Control-Allow-Origin', '*');
+// 	res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+// 	res.set('Access-Control-Allow-Headers', 'Content-Type');
+// 	next();
+// });
+
+app.options('*', cors())
 
 app.use(bodyParser.json());
 app.use('/v1', require('./routes/v1'));
