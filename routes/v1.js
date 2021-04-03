0949e4c7171784091c1ecaa7b964214e1058d922
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-03 00:46:45 -0700

    Added users routes

--- routes/v1.js
+++ routes/v1.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.use('/task', require('./v1/task'));
 router.use('/taskList', require('./v1/taskList'));
 router.use('/admin', require('./v1/admin'));
+router.use('/user', require('./v1/user'));
 
 module.exports = router;
--- routes/v1/user.js
+++ routes/v1/user.js
@@ -2,3 +2,27 @@
 
 const express = require('express');
 const router = express.Router({mergeParams: true});
+
+router.post('/', (req, res) => {
+  res.status(201).send(`POST:
+  ${req.body.username}
+  ${req.body.password}
+  ${req.body.name}
+  ${req.body.isAdmin}`);
+});
+
+router.get('/:userID', (req, res) => {
+  res.status(200).send('GET /userID ' + req.params.userID);
+});
+
+router.post('/login', (req, res) => {
+  res.status(200).send(`POST /login:
+  \n${req.body.username}
+  \n${req.body.password}`);
+});
+
+router.post('/logout', (req, res) => {
+  res.status(200).send('Logged out successfully');
+});
+
+module.exports = router;
