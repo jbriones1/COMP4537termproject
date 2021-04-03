c041c4ab054cca63fdd17d282b46059d90a13fb1
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-03 16:59:25 -0700

    Changing CORS again again

--- server.js
+++ server.js
@@ -2,20 +2,20 @@
 
 const express = require('express');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
-app.all('*', (req, res, next) => {
+// app.all('*', (req, res, next) => {
 
-	res.set('Access-Control-Allow-Origin', '*');
-	res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-	res.set('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-});
+// 	res.set('Access-Control-Allow-Origin', '*');
+// 	res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+// 	res.set('Access-Control-Allow-Headers', 'Content-Type');
+// 	next();
+// });
 
 
-// app.use('*', cors());
+app.use('*', cors());
 app.use(bodyParser.json());
 app.use('/v1', require('./routes/v1'));
 
