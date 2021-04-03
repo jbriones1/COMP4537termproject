b644fcb692e872b22dc52f1420f0c6eaab387197
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-03 00:55:08 -0700

    Removed some unnecessary code

--- server.js
+++ server.js
@@ -8,22 +8,4 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use('/v1', require('./routes/v1'));
 
-/************************ USER ENDPOINTS ************************/
-
-// app.post('/user', (req, res) => {
-
-// });
-
-// app.post('/user', (req, res) => {
-  
-// });
-
-// app.get('/user', (req, res) => {
-
-// });
-
-// app.get('/user', (req, res) => {
-
-// });
-
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
