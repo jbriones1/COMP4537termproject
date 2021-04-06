921b6916dbc3d049bb21eafde696fa016254450f
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-05 18:13:03 -0700

    Removed unnecessary code

--- server.js
+++ server.js
@@ -6,15 +6,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
-// app.all('*', (req, res, next) => {
-
-// 	res.set('Access-Control-Allow-Origin', '*');
-// 	res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-// 	res.set('Access-Control-Allow-Headers', 'Content-Type');
-// 	next();
-// });
-
-
 app.use('*', cors());
 app.use(bodyParser.json());
 app.use('/v1', require('./routes/v1'));
