be0fae24d7e526aca64e020c1f0156e1bd2326f3
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-06 13:23:51 -0700

    Moved login and logout out of the API

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -8,14 +8,4 @@ router.post('/', queries.createUser);
 
 router.get('/:userID', queries.getUserByID);
 
-router.post('/login', (req, res) => {
-  res.status(200).send(`POST /login:
-  \n${req.body.username}
-  \n${req.body.password}`);
-});
-
-router.post('/logout', (req, res) => {
-  res.status(200).send('Logged out successfully');
-});
-
 module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,17 @@ dotenv.config();
 app.use('*', cors());
 app.use(bodyParser.json());
 
+
+app.post('/login', (req, res) => {
+  res.status(200).send(`POST /login:
+  \n${req.body.username}
+  \n${req.body.password}`);
+});
+
+app.post('/logout', (req, res) => {
+  res.status(200).send('Logged out successfully');
+});
+
 app.use('/v1', require('./routes/v1'));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
