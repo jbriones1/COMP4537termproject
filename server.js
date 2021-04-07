9d271ceeb0a19c075436b91122ed9aa01263363f
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-06 21:58:36 -0700

    Removed body-parser dependency

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 
 app.use('*', cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use('/', require('./routes/authentication'));
 app.use('/v1', require('./routes/v1'));
 
