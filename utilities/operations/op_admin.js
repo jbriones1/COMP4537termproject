edd6f3d2400b1740afe896f263d0fa84f59b1147
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-06 20:41:17 -0700

    Promisified all existing queries

--- utilities/operations/op_admin.js
+++ utilities/operations/op_admin.js
@@ -3,12 +3,10 @@ const sql = require('../db');
 exports.getAdminStats = (req, res) => {
   const sql_statement = 'SELECT * FROM apicount';
 
-  sql.db.query(sql_statement, (err, result) => {
-    if (err) {
-      res.sendStatus(500);
-      return;
-    }
-
-    res.status(200).json(result);
-  });
+  sql.db.query(sql_statement)
+    .then(result => {
+      res.status(200).json(result);
+    }).catch(err => {
+      return res.sendStatus(500);
+    });
 };
