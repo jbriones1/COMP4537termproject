d8bb051a15e647a7ad8cb275a6ab7cc8cc0df59a
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-08 02:12:55 -0700

    Task comments

--- utilities/operations/op_task.js
+++ utilities/operations/op_task.js
@@ -1,10 +1,21 @@
 const sql = require('../db');
 
+/**
+ * Adds a task.
+ * 
+ * 200: success
+ * 400: invalid body
+ * 500: database error
+ * 
+ * @param {Object} req request
+ * @param {Object} res result
+ * @returns status code
+ */
 const addTask = (req, res) => {
 
   const { taskName, taskDescription, isComplete, taskListID } = req.body;
 
-  if (!taskName || !taskDescription || isComplete == null) {
+  if (!taskName || !taskDescription || isComplete == null || taskListID == null) {
     return res.sendStatus(400);
   }
 
