02e6aca60d484fa5b832f1b4415ca37da92983c1
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-08 01:54:23 -0700

    Fetching today's date works

--- utilities/operations/op_task.js
+++ utilities/operations/op_task.js
@@ -1,26 +1,17 @@
 const sql = require('../db');
 
 const addTask = (req, res) => {
-  const taskName = req.body.taskName;
-  const taskDescription = req.body.taskDescription;
-  const isComplete = req.body.isComplete;
-  const taskListID = req.body.taskListID;
+
+  const { taskName, taskDescription, isComplete, taskListID } = req.body;
 
   if (!taskName || !taskDescription || isComplete == null) {
     return res.sendStatus(400);
   }
 
-  const values = {
-    taskListID,
-    taskName,
-    taskDescription,
-    isComplete
-  }
-
   const query =
     `INSERT INTO task SET ?`;
   
-  sql.db.query(query, [values])
+  sql.db.query(query, [{ taskName, taskDescription, isComplete, taskListID }])
   .then(result => res.sendStatus(200))
   .catch(err => res.sendStatus(500));
 }
--- utilities/operations/op_taskList.js
+++ utilities/operations/op_taskList.js
@@ -2,19 +2,17 @@ const sql = require('../db');
 
 const addTaskList = (req, res) => {
 
-  const values = {
-    userID: req.body.userID,
-    date: req.body.date
-  }
+  const { userID, date } =  req.body;
 
-  if (!values.userID || !values.date) return res.sendStatus(400);
+
+  if (!userID || !date) return res.sendStatus(400);
 
   const query = 
     `INSERT INTO taskList
      SET ?`;
   
-  sql.db.query(query, [values])
-  .then(result => res.sendStatus(201))
+  sql.db.query(query, [{ userID, date }])
+  .then(_ => res.sendStatus(201))
   .catch(err => {
     console.log(err);
     res.sendStatus(500);
@@ -40,8 +38,43 @@ const deleteTaskList = (req, res) => {
   });
 }
 
-const getTaskList = (req, res) => {
+const getTaskList = async (req, res) => {
+
+  const { userID, date } = req.body;
+
+  if (!userID, !date) return res.sendStatus(400)
   
+  try {
+
+    const q1 = 
+      `SELECT taskListID 
+       FROM taskList 
+       WHERE userID = ? 
+             AND date = ?`;
+
+    const [taskList, _1] = await sql.db.query(q1, [userID, date]);
+
+    if (taskList.length < 1) return res.sendStatus(404);
+
+    const q2 = 
+      `SELECT taskID,taskName,taskDescription,isComplete
+       FROM task 
+       WHERE taskListID = ?`;
+
+    const [tasks, _2] = await sql.db.query(q2, [taskList[0].taskListID]);
+
+    const results = {
+      taskListID: taskList[0].taskListID,
+      tasks: tasks
+    }
+
+    res.status(200).json(results);
+
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500);
+  }
+
 }
 
 const getTaskListYesterday = (req, res) => {
